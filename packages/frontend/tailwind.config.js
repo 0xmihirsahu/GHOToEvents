0d8882d41af57bf39a5b302a1b4a6831fe75bc8a
Mihir Sahu <0xmihirsahu@gmail.com>
2024-01-20 09:14:48 +0530

    Added Create event page and css fix

--- packages/frontend/tailwind.config.js
+++ packages/frontend/tailwind.config.js
@@ -12,6 +12,12 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontFamily: {
+        sans: ['Inter var', 'Inter', 'ui-sans-serif', 'system-ui'],
+        mono: ['JetBrains Mono', 'ui-monospace', 'SFMono-Regular'],
+        kanit: ['kanit'],
+        pixelify: ['Pixelify Sans'],
+      },
     },
   },
   plugins: [],
